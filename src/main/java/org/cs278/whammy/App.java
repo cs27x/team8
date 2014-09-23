dd5da2cea3345a087210daa60bf4c6996c7ba76f
Jules White <jules.white@gmail.com>
2014-09-23 10:07:25 -0500

    - Initial commit of project skeleton

---
+++ src/main/java/org/cs278/whammy/App.java
@@ -0,0 +1,10 @@
+package org.cs278.whammy;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/org/cs278/whammy/test/AppTest.java
@@ -0,0 +1,14 @@
+package org.cs278.whammy.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
