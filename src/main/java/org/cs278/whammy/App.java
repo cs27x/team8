0cd16cfc2b6ba4eed6259b607527ff452f1b05b5
Jules White <jules.white@gmail.com>
2014-09-27 07:06:17 -0500

    Whammy!

--- src/main/java/org/cs278/whammy/App.java
+++ src/main/java/org/cs278/whammy/App.java
@@ -2,7 +2,7 @@ package org.cs278.whammy;
 
 public class App {
 
-        private boolean foo = false;
+        private boolean foo = true;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
